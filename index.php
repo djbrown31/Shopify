fe594a7a4a9bcdbf6b177e1a44ee5fb29eb4793a
Dan Brown <brown.danny.j@gmail.com>
2020-11-06 13:01:19 -0600

    commit

--- index.php
+++ index.php
@@ -1,64 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset='utf-8'>
-	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-	<meta name='viewport' content='width=device-width, initial-scale=1'>
-	<title>Office Blankets
-	</title>
-	<!--CSS-->
-	<!--Bootstrap-->
-	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
-	<!--Optional Bootstrap Theme-->
-	
-	<!--FontAwesome-->
-	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
-	
-	<!--Custom CSS-->
-	<link rel='stylesheet' href='css/style.css'>
-</head>
-<body>
+<?php
+	session_start();
+	include "inc/head.inc.php";
+?>
+
 	<!--HEADER-->
 	<header>
 		<div class='container-fluid'>
 			<div class='jumbotron text-center'>
 				<h1>Office Blankets</h1>
+				<?php
+					if(isset($_SESSION['username'])){
+						echo "<p>Welcome back, ".$_SESSION['username']."!</p>";
+					}
+					?>
 				<p>Offering a wide range of Customizable Office Blankets</p>
 			</div>
 		</div>
 	</header>
-	
-	<nav class='navbar navbar-inverse navbar-fixed-top'>
-		<div class='container-fluid'>
-			<header class='navbar-header'>
-				<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>
-					<span class='icon-bar'></span>
-					<span class='icon-bar'></span>
-					<span class='icon-bar'></span>
-				</button>
-				<a class='navbar-brand' href='index.php'>Office Blankets Home</a>
-			</header>
-			<div class='collapse navbar-collapse' id='myNavbar'>
-				<ul class='nav navbar-nav'>
-					<li><a href='#'>Products</a></li>
-					<li><a href='#'>Deals</a></li>
-				</ul>
-				<ul class='nav navbar-nav navbar-right'>
-					<li><a href='#'><span class='glyphicon glyphicon-user'></span>Sign In</a></li>
-					<li><a href='#'><span class='glyphicon glyphicon-shopping-cart'></span>Cart</a></li>
-					<li>
-						<form class='navbar-form' action='#' method='POST'>
-							<input type='text' class='form-control' placeholder='Search for a Product'>
-							<button type='submit' class='btn btn-primary'><span class='glyphicon glyphicon-search'></span></button>
-						</form>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+
 	<!--MAIN-->
-	<main>
-		<div class='container'>
+	<main class='container'>
+		<div>
 			<div class='row'>
 			<div class="col-md-3">
 			<p class="lead">Product Range</p>
@@ -89,10 +51,10 @@
 								<img class="slide-image" src="car1.jpg" alt="">
 							</div>
 							<div class="item active">
-								<img class="slide-image" src="car1.jpg" alt="">
+								<img class="slide-image" src="car2.jpg" alt="">
 							</div>
 							<div class="item active">
-								<img class="slide-image" src="car1.jpg" alt="">
+								<img class="slide-image" src="car3.jpg" alt="">
 							</div>
 							</div>
 							<a class="left carousel-control href="#carousel-example-generic" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
@@ -104,7 +66,7 @@
 			</div>
 			<div class="row">
 				<div class="thumbnail">
-					<img src="http://placehold.it/320x150" alt="Thumbnail Image">
+					<img src="C:\xampp2\htdocs\Shopify\img\car1.jpg" alt="Thumbnail Image">
 					<div class="caption">
 						<h4 class="pull-right">$35,000</h4>
 						<h4><a href="#">A Product</a></h4>
@@ -122,7 +84,7 @@
 					</div>
 				</div>
 				<div class="thumbnail">
-					<img src="http://placehold.it/320x150" alt="Thumbnail Image">
+					<img src="C:\xampp2\htdocs\Shopify\img\car2.jpg" alt="Thumbnail Image">
 					<div class="caption">
 						<h4 class="pull-right">$35,000</h4>
 						<h4><a href="#">A Product</a></h4>
@@ -140,7 +102,7 @@
 					</div>
 				</div>
 				<div class="thumbnail">
-					<img src="http://placehold.it/320x150" alt="Thumbnail Image">
+					<img src="C:\xampp2\htdocs\Shopify\img\car3.jpg" alt="Thumbnail Image">
 					<div class="caption">
 						<h4 class="pull-right">$35,000</h4>
 						<h4><a href="#">A Product</a></h4>
@@ -164,30 +126,6 @@
 		</div>
 	</main>
 	<!--FOOTER-->
-	<footer class='text-center'>
-		<nav class='navbar navbar-inverse'>
-			<div class='container-fluid'>
-				<ul class='nav navbar-nav'>
-					<li class='navbar-text'>Copyright &copy; Shaw Academy</li>
-					<li><a href='#'><i class='fa fa-facebook'></i></a></li>
-					<li><a href='#'><i class='fa fa-google'></i></a></li>
-					<li><a href='#'><i class='fa fa-twitter'></i></a></li>
-				</ul>
-				<ul class='nav navbar-nav navbar-right'>
-					<li><a href='#'>Site Map</a></li>
-					<li><a href='#'>Contact Us</a></li>
-					<li><a href='#'>Terms &amp; Conditions</a></li>
-				</ul>
-			</div>
-		</nav>
-	</footer>
-	<!--JAVASCRIPT-->
-	<!--JQuery-->
-	<script src='https://code.jquery.com/jquery-3.1.0.min.js' integrity='sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=' crossorigin='anonymous'></script>
-	
-	<!--Bootstrap JS-->
-	<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>
-	
-	<!--Custom JS-->
-</body>
-</html>
+	<?php
+		include "inc/foot.inc.php";
+	?>
